58e5ecf32f31c1385da7d24dc4bfa27238d9e716
Juncheng Wu <wuj@aldenv100.allegheny.edu>
2017-10-11 16:33:39 -0400

    LAB 6 FINAL

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,28 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
+
+/**
+ * This class produces ManipulateDna program output.
+ * The program will display some randomly changed strings.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Gregory Kapfhammer build the basic code and Lancaster Wu edited
+ */
 
 public class ManipulateDna {
 
+  /**
+ * This is part of lab 6 form Compsci 111.
+ * students suppose to make a program called ManipulateDna, and
+ * the output should be Randomly changed DNA strings.
+ */
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Lancaster Wu " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -22,7 +37,6 @@ public class ManipulateDna {
     }
     // Step One: Read in and display the chosen DNA string
     String dna =  scanner.nextLine();
-    System.out.println("Lancaster Wu " + new Date());
     System.out.println("Okay, I am going to manipulate the DNA string: \"" + dna + "\".");
     System.out.println();
     // Step Two: Compute the complement of the DNA String
@@ -31,15 +45,38 @@ public class ManipulateDna {
     String complement2 = complement1.replace("c","G");
     String complement3 = complement2.replace("t","A");
     String complement4 = complement3.replace("g","C");
-    System.out.println("Complement of " + dna.toUpperCase() + " is " + complement4);
+    String dna2 = dna.toUpperCase();
+    System.out.println("Complement of " + dna2 + " is " + complement4);
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
     Random generator = new Random();
+    int num1;
+    num1 = generator.nextInt(dna2.length());
+    int num2;
+    num2 = generator.nextInt(dna2.length());
+    char char1;
+    char1 = dna2.charAt(num2);
+    String insert = dna2.substring(0,num1) + char1 + dna2.substring(num1,dna2.length());
+    System.out.println("Inserting " + char1 + " at position " + num1 + " gives " + insert);
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
+    int num3;
+    num3 = generator.nextInt(dna2.length());
+    String delete = dna2.substring(0,num3) + dna2.substring(num3 + 1,dna2.length());
+    System.out.println("Deleting from position " + num3 + " gives " + delete);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    int num4;
+    int num5;
+    num4 = generator.nextInt(dna2.length());
+    num5 = generator.nextInt(dna2.length());
+    char char2;
+    char2 = dna2.charAt(num4);
+    char char3;
+    char3 = dna2.charAt(num5);
+    String replace = dna2.replace(char2,char3);
+    System.out.println("Changing position " + num4 + " gives " + replace);
     // Step Six: Display a final thankyou message
-
+    System.out.println();
+    System.out.println("Thanks for using the ManipulateDna program.");
+    System.out.println("Have an awesome day.");
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,28 @@
-# Reflection by
+# Reflection by Lancaster Wu
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to write a file called `ManipulateDNA`. The purpose of
+this lab is to learn how to use `random int` to program. The output should have
+my name and the date, the short discription to the input DNA string.Then the
+output need a complement of the input DNA String, a new DNA String which has a
+inserted randomly chosen DNA letter, a new DNA String which has a randomly
+deleted DNA letter, a new DNA String which as a DNA letter changed randomly to
+another DNA letter, and a `Thanks` comment at the end.
+
+I was struggled with how to replace the complement letter at first, then I
+realize that I can keep input in the lowercase and the complement in the
+uppercase, and replace each letter step by step. I was then struggled with the
+radom generator. I tried many times but somehow the `gradle run` would not show
+me the output. By checking my codes sentence by sentence, I found that I did
+not import the `java.util.Random;` before I use `Random generator`. Everything
+works fine after I solve this problem.
+
+For the deleting DNA letter String, I did not know how can I code the string,
+so I searched online and used `strWithoutChar = new
+StringBuilder(yourString).deleteCharAt(yourIndex).toString();`. The class
+`StrungBuilder` is not what we learned in class, so I asked Professor
+Kapfhammer for the way we suppose to know by using what we learned in class. I
+soon realize it is the same to the `str.substring` code I used for inserting
+DNA letter String.Also, I learned that for better format of coding, I should
+use `str.length` instead of specific number for the input we have already, then
+this program can also be used on other inputs with out error.
+
